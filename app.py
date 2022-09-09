4711e67699a6450474d7274e80a61d02e199d104
Aaron Podell <aaronpodell@gmail.com>
2022-09-08 23:15:28 -0400

    say hi to greg and dan

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 @app.route('/')
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
-    return 'Hello, World!'
+    return 'Hey Greg and Dan! Let Draft Draft Development begin!'
 
 @app.route('/success/<name>')
 def success(name):
