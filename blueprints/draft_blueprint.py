4d592d31d4ec9ba65c6a91e45a0e89442965bb1c
Greg Nelson <48419435+gjnelson112@users.noreply.github.com>
2023-01-31 23:56:30 -0500

    Best + Worst Pick Leaderboards (#17)
    * query for calculating_best picks by differential. WIP
    * add in worst leaderboard and some styling
    * add pythonPath helper notes to readme
    
    ---------
    
    Co-authored-by: gjnelson112
    Co-authored-by: ajpodell <ajpodell@users.noreply.github.com>

--- blueprints/draft_blueprint.py
+++ blueprints/draft_blueprint.py
@@ -11,6 +11,7 @@ from flask import (
     url_for,
 )
 from flask_login import current_user, login_required
+from sqlalchemy import desc, asc, nulls_last
 
 from models.base import db
 from models.draft import Draft
@@ -20,6 +21,7 @@ from models.user import User
 
 draft = Blueprint('draft', __name__)
 
+
 @draft.before_request
 def check_valid_login():
     """ Detect if user is logged in as the default - basically equivalent to `@login_required`
@@ -27,17 +29,36 @@ def check_valid_login():
     """
     # ensure user is logged in
     login_valid = current_user.is_authenticated
-    if (request.endpoint and 
-        'static' not in request.endpoint and 
-        not login_valid and 
-        not getattr(draft.view_functions.get(request.endpoint, {}), 'is_public', False) ) :
+    if (request.endpoint and
+            'static' not in request.endpoint and
+            not login_valid and
+            not getattr(draft.view_functions.get(request.endpoint, {}), 'is_public', False)):
         return render_template('login.html', next=request.endpoint)
+
 
 def public_endpoint(function):
     """ decorator to make public -- probably want to functools wraps this """
     function.is_public = True
     return function
 
+
+def generate_leaderboard(leaderboard_type):
+    differential_column_label = 'pick_differential'
+    sort_dir = asc(differential_column_label) if leaderboard_type == "worst" else desc(differential_column_label)
+
+    rows = db.session.query(Player.player_name_first,
+                            Player.player_name_last,
+                            User.username,
+                            (Selection.draftdraft_selection - Player.nfl_draft_pick).label(differential_column_label))
+        .join(Selection,
+              Selection.player_id == Player.player_id)
+        .join(User,
+              Selection.selecting_team_id == User.user_id)
+        .order_by(nulls_last(sort_dir))
+        .limit(3)
+        .all()
+
+    return rows
 
 def next_up():
     """ returns team id of the next team up"""
@@ -58,7 +79,7 @@ def next_up():
 
     return next(team for team in all_teams if team.pick.pick_order == next_pick)
 
- 
+
 @draft.route('/home')  # maybe theres a better way to do url_for('/')
 @draft.route('/')
 def home():
@@ -66,7 +87,7 @@ def home():
     # kind of want to put home in a dedicated "make pick" page - but thats a later problem
     pick_order = db.session.query(Draft).order_by(Draft.pick_order)
 
-    #TODO: have an "is_picked" property - can probably do this as a hybrid python thing on the model 
+    # TODO: have an "is_picked" property - can probably do this as a hybrid python thing on the model
     # looking at the draft
     players = db.session.query(Player).order_by(Player.player_id).all()
 
@@ -78,6 +99,7 @@ def home():
 def players():
     players = db.session.query(Player).all()
     return list(player.__repr__() for player in players)
+
 
 @draft.route('/players', methods=['POST'])
 def add_player():
@@ -92,6 +114,7 @@ def add_player():
 
     return redirect(url_for('draft.home'))
 
+
 @draft.route('/add_selection', methods=['POST'])
 def add_selection():
     """ todo: just delete this?"""
@@ -104,6 +127,7 @@ def add_selection():
     db.session.commit()
     return {}
 
+
 @draft.route('/draft')  # would be cool to have a "league" template var
 # @login_required
 def view_draft():
@@ -113,7 +137,12 @@ def view_draft():
     # get all teams
     teams = db.session.query(User).all()
 
-    return render_template('draft.html', selections=all_picks, teams=teams)
+    return render_template('draft.html',
+                           selections=all_picks,
+                           teams=teams,
+                           best_selections=generate_leaderboard('best'),
+                           worst_selections=generate_leaderboard('worst'))
+
 
 @draft.route('/draft_player', methods=['POST'])
 def draft_player():
@@ -138,7 +167,8 @@ def draft_player():
         flash('Not your pick', 'error')
         return redirect(url_for('draft.home'))
 
-    selection = Selection(player_id=selected_player_id, selecting_team_id=next_pick.user_id)  # wasnt working with team_id
+    selection = Selection(player_id=selected_player_id,
+                              selecting_team_id=next_pick.user_id)  # wasnt working with team_id
     db.session.add(selection)
     db.session.commit()
     return redirect(url_for('draft.view_draft'))
@@ -149,6 +179,7 @@ def score_players():
     """ as of yet unused view for a dedicated scoring """
     return render_template('score_players.html')
 
+
 @draft.route('/reset_draft', methods=['POST'])
 def reset_draft():
     if not current_user.is_authenticated or not current_user.is_admin:
@@ -158,7 +189,6 @@ def reset_draft():
     db.session.execute(' TRUNCATE table public.selection; TRUNCATE table public.draft;')
     db.session.execute('ALTER SEQUENCE selection_seq RESTART WITH 1')
     db.session.commit()
-
 
     teams = db.session.query(User).all()
     random.shuffle(teams)
@@ -188,6 +218,7 @@ def score_player():
     db.session.query(Player).filter_by(player_id=player_id).update({Player.nfl_draft_pick: nfl_draft_pick})
     db.session.commit()
     return redirect(url_for('draft.home'))
+
 
 @draft.route('/admin_tools')
 def admin_tools():
