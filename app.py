13df74d6da1c538403c4fb8efb9d2e4c009bc566
gjnelson112 <gjnelson112@gmail.com>
2023-04-10 03:00:24 -0400

    testing deploys

--- app.py
+++ app.py
@@ -63,14 +63,14 @@ def create_app():
     return app
 
 
+app = create_app()
+
 def run_app():
     # app = create_app()
     # app.run(debug=app.config.get('DEBUG'))
-    app = create_app()
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
 
-# app = create_app()
 # port = int(os.environ.get("PORT", 5000))
 # app.run(host='0.0.0.0', port=port)
 
